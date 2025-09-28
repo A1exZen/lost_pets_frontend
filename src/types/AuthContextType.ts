00d67beba431c52bd28ee1d8fa130a0cd4f0be5c
Alex <alexcomp2021@gmail.com>
2025-09-28 19:25:05 +0300

    feat: add User & Pet types

---
+++ src/types/AuthContextType.ts
@@ -0,0 +1,8 @@
+import type {User} from "./User.ts";
+
+export interface AuthContextType {
+	user: User | null;
+	login: (email: string, password: string) => Promise<void>;
+	register: (email: string, password: string, name: string) => Promise<void>;
+	logout: () => void;
+}
---
+++ src/types/Pet.ts
@@ -0,0 +1,11 @@
+export interface Pet {
+	id: number;
+	userId: number;
+	name: string;
+	type: string;
+	breed: string;
+	photo: string;
+	description: string;
+	contacts: string;
+	dateLost: string;
+}
